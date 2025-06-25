9cb36c33430548db150ebb22e814241cdc81a3f7
Joeyhofman <joey.hofman@windesheim.nl>
2025-06-25 16:44:49 +0200

    added installation of pyenv

---
+++ runs/insatll_pyenv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PREFIX="[PYENV]: "
+
+echo "$PREFIX updating repositories..."
+sudo apt update
+
+echo "$PREFIX updating dependancies..."
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev git
+
+echo "$PREFIX downloading pyenv..."
+curl https://pyenv.run | bash
+
+
+echo "$PREFIX cofnigureing shell for pyenv..."
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
