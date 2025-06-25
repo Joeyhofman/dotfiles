33b91b6f0f1622b8312afbf6b19b6629da341fca
Joeyhofman <joey.hofman@windesheim.nl>
2025-06-25 16:57:33 +0200

    linking dotfiles

---
+++ runs/symlink_dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PREFIX="[SYmlinking dotfiles]: "
+
+echo "$PREFIX Symlinking .tmux.conf"
+ln -s ~/personal/dev_setup/dotfiles/.tmux.conf ~/.tmux.conf
+
+
+echo "$PREFIX Symlinking .bashrc"
+ln -s ~/personal/dev_setup/dotfiles/.bashrc ~/.bashrc
+
+
+echo "$PREFIX Symlinking .zshrc"
+ln -s ~/personal/dev_setup/dotfiles/.zshrc ~/.zshrc
+
