360a457923cb88a5a91bcc4ff73308f33ea504ef
Joeyhofman <joey.hofman@windesheim.nl>
2025-07-29 15:46:18 +0200

    zoxide

--- runs/symlink_dotfiles.sh
+++ runs/symlink_dotfiles.sh
@@ -13,3 +13,6 @@ ln -s ~/personal/dev_setup/dotfiles/.bashrc ~/.bashrc
 echo "$PREFIX Symlinking .zshrc"
 ln -s ~/personal/dev_setup/dotfiles/.zshrc ~/.zshrc
 
+echo "$PREFIX Symlinking zoxide db.zo..."
+ln -s ~/personal/dev_setup/zoxide/db.zo ~/.local/share/zoxide/db.zo
+
